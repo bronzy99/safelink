8abf79cfe6700cd6dd174fe8889a8f23526edb09
bronzy99 <43916940+bronzy99@users.noreply.github.com>
2020-04-01 08:24:52 +0100

    Update sitemap.js

--- sitemap.js
+++ sitemap.js
@@ -223,7 +223,7 @@ function displayToc(filter) {
    }
    tocTable += '</table>';
    if (numDisplayed == postTitle.length) {
-      var tocNote = '<span class="toc-note">Displaying all ' + postTitle.length + ' posts<br/></span>'; }
+      var tocNote = '<span class="toc-note">Latest Articles<br/></span>'; }
    else {
       var tocNote = '<span class="toc-note">Displaying ' + numDisplayed + ' posts labeled \'';
       tocNote += postFilter + '\' of '+ postTitle.length + ' posts total<br/></span>';
